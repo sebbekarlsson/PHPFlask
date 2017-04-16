8f3426160e7cffea4fde9cb8ed7ad03b86a5d2e9
Sebastian Karlsson] <sebbekarlsson97@gmail.com>
2017-04-16 17:19:33 +0200

    first commit

---
+++ src/App.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The routing application
+ */
+class App {
+    function __construct() {
+    
+    }
+
+    /**
+     * Runs the application, will start
+     * parsing requests.
+     */
+    public function run() {
+    
+    }
+
+    /**
+     * Registers a blueprint that will be used as a route.
+     *
+     * @param Blueprint $blueprint;
+     */
+    public function register_blueprint($blueprint) {
+        
+    }
+}
