7c332d591d8a4e243c44025144e481da7d2f291e
Sebastian Karlsson] <sebbekarlsson97@gmail.com>
2017-04-16 23:36:13 +0200

    added comments and updated utils.php

--- src/utils.php
+++ src/utils.php
@@ -3,10 +3,19 @@
 $loader = new Twig_Loader_Filesystem(TEMPLATE_DIRECTORY);
 $twig = new Twig_Environment($loader, []);
 
+/**
+ * Render a twig template.
+ * This is in place of Jinja which is used in Flask.
+ *
+ * @param String $template_path
+ * @param Array $args
+ *
+ * @return String
+ */
 function render_template($template_path, $args=[]) {
     global $twig;
 
-    if (empty($args)) {
+    if (empty($args) && !is_array($args)) {
         $args = [];
     }
 
